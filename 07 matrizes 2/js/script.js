767ee4add52fb310317856d7d2af9320f5b48553
Miguelmu <135176438+Miguelmu@users.noreply.github.com>
2023-06-07 22:28:14 +0000

    modified:   07 matrizes 2/js/script.js

--- 07 matrizes 2/js/script.js
+++ 07 matrizes 2/js/script.js
@@ -10,8 +10,8 @@ Verifique se os números inseridos são inteiros positivos. Caso contrário,
 Divida os dois números inteiros positivos;
 
 Imprima na tela o resultado da divisão.*/
-
-/* let numeros1 = prompt("insira o primeiro numero inteiros positivos");
+/*
+ let numeros1 = prompt("insira o primeiro numero inteiros positivos");
 
 let numeros2 = prompt("insira o segundo numero inteiros positivos");
   
@@ -24,7 +24,7 @@ console.log(resultado)
 else{
  console.log("insira novamente") 
 }
-*/
+
 
 let numeros = [];
 let numero1 = solicitaPrimeiroNumero();
@@ -32,4 +32,37 @@ let numero2 = solicitaSegundoNumero();
 
 function solicitaPrimeiroNumero(){
     
-};
+};      */
+
+//o vetor - armazenar os valores dos numeros
+//funções - definir o valor dos numeros
+
+
+let numeros = [];
+ numeros[0] = solicitaPrimeiroNumero();
+ numeros[1] = solicitaSegundoNumero();
+
+function solicitaPrimeiroNumero(){
+    let numeros1 = prompt("insira o primeiro numero inteiros positivos");
+    if (numeros1 < 0){
+        alert("Escolha outro numero")
+        solicitaPrimeiroNumero()
+    }  
+    else{
+        return numeros1
+    }
+}
+   
+function solicitaSegundoNumero(){ 
+    let numeros2 = prompt("insira o segundo numero inteiros positivos");
+    if (numeros2 < 0){
+        alert("Escolha outro numero")
+        solicitaSegundoNumero()
+    }  
+    else{
+        return numeros2
+    }
+}
+console.log(numeros[0]/numeros[1])
+
+let result = numeros[0]/numeros[1]
